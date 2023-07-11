9d0a8c98bdfc77844602f8a76f45a0db81ec23b4
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2023-07-11 22:31:34 +0200

    First commit

---
+++ src/components/intro.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./intro.css";
+import myImg from "/Users/Admin/Desktop/Projects/website1/src/img/imgg.jpg";
+
+const intro = () => {
+  return (
+    <div className="intro">
+      <h1>Fixed Top Navigation Bar</h1>
+      <h2>Scroll this page to see the effect</h2>
+      <h2>
+        The navigation bar will stay at the top of the page while scrolling
+      </h2>
+
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+      <p>Some text some text some text some text..</p>
+    </div>
+  );
+};
+
+export default intro;
---
+++ src/components/navigationBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./navigationBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const nav = () => {
+  return (
+    <div className="n">
+      <div className="n-wrapper">
+        <ul className="n-items">
+          <li className="n-item">HOME</li>
+          <li className="n-item">ABOUT US</li>
+          <li className="n-item">SHOP</li>
+          <li className="n-item">
+            BOOK STALL <FontAwesomeIcon icon="fa fa-user" />
+          </li>
+          <li className="n-item">CONTACTS</li>
+        </ul>
+
+        <div className="info-wrapper">
+          <div className="i-event">
+            <div className="h">H</div>
+            <div className="h1">DJ NATION</div>
+            <div className="h2">ANNUAL BASH</div>
+            <div className="i-vector"></div>
+          </div>
+        </div>
+
+        <div className="i-details">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+          <button className="i-button">READ MORE</button>
+          <div className="i-date">
+            <p>20</p>
+            <h3>23</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default nav;
